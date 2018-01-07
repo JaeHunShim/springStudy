825b592051e368c9b7c9b5d98a26aeb0bd6b5e70
심재훈 <@LAPTOP-777HJK2C>
2018-01-07 18:28:10 +0900

    다오를 빼먹고 올림

--- overware/src/main/java/com/overware/persistence/MemberDAO.java
+++ overware/src/main/java/com/overware/persistence/MemberDAO.java
@@ -9,6 +9,6 @@ public interface MemberDAO {
 	public void insertMember(MemberVO vo);
 	// 회원 정보 일거오기
 	public MemberVO readMember(String userid)throws Exception;
-	//회원 id와 패스워드 읽어 들여오기
+	//회원 id와 패스워드 읽어 들여오기 
 	public MemberVO readWithPW(String userid,String userpw)throws Exception;
 }
