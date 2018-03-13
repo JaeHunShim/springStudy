5dee5b1e5bad85f78771004c6c7ed2d1b82a46ff
LAPTOP-J0CSRS4B\jaehun <jaehuniya@gmail.com>
2018-03-13 17:20:01 +0900

    spring+jQuery+JSon 이용해서 자동완성 기능 해보기

---
+++ overware/src/main/java/com/overware/spring/SpringMVCController.java
@@ -0,0 +1,35 @@
+package com.overware.spring;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SpringMVCController {
+    @RequestMapping(value = "/autocomplate", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
+
+    model.addAttribute("message", "Spring MVC jQuery" + " Autocomplete");
+    
+    return "autocomplate";
+
+    }
+    
+
+    @RequestMapping(value = "/getMachedNames.web", method = RequestMethod.GET)
+    public @ResponseBody
+    List<String> getMachedNames(@RequestParam("term") String name){
+    
+    List<String> matchName = DataCache.getName(name);
+
+    return matchName;
+    }
+}
